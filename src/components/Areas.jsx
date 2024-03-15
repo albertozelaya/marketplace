f878a0a472634d8682ab835f112bbf668761af0b
Alberto Zelaya <albertozelaya5@yahoo.com>
2024-03-14 22:07:51 -0600

    avance 14

--- src/components/Areas.jsx
+++ src/components/Areas.jsx
@@ -1,17 +1,37 @@
 const Areas = () => {
   return (
-    <div className="my-4">
-      <div className="mx-auto px-3 md:px-0 w-full py-[50px]">
-        <div className="max-w-[1240px] mx-auo">
-          <div className="grid md:grid-cols-5">
-            <div className=" bg-BG_DARK">
+    <div className="-mb-20 -mx-5 md:my-4 md:mx-4">
+      <div className="mx-auto md:px-0 w-full py-[50px]">
+        <div className="md:p-8 max-w-[1240px] mx-auo grid grid-cols-3 md:grid-cols-4 md:gap-8 gap-1.5">
+          <div className="overflow-hidden  md:p-2 rounded-md">
+            <div className="rounded-full">
               <img
-                className="h-40 w-full object-cover"
                 src="https://www.banhcafe.hn/_next/image?url=%2Fannouncement-4.webp&w=640&q=75"
-                alt="banca"
+                className="h-28 md:h-36 w-full object-cover rounded-full"
+                alt=""
               />
             </div>
-            <p>Partes y accesorios</p>
+            <p className="px-5 py-2">Partes y accesorios</p>
+          </div>
+          <div className="overflow-hidden  md:p-2 rounded-md">
+            <div className="rounded-full">
+              <img
+                src="https://www.banhcafe.hn/_next/image?url=%2Fannouncement-4.webp&w=640&q=75"
+                className="h-28 md:h-36 w-full object-cover rounded-full"
+                alt=""
+              />
+            </div>
+            <p className="px-5 py-2">Partes y accesorios</p>
+          </div>
+          <div className="overflow-hidden  md:p-2 rounded-md">
+            <div className="rounded-full">
+              <img
+                src="https://www.banhcafe.hn/_next/image?url=%2Fannouncement-4.webp&w=640&q=75"
+                className="h-28 md:h-36 w-full object-cover rounded-full"
+                alt=""
+              />
+            </div>
+            <p className="px-5 py-2">Partes y accesorios</p>
           </div>
         </div>
       </div>
@@ -20,3 +40,9 @@ const Areas = () => {
 };
 
 export default Areas;
+
+/* <img
+                className=""
+                src="https://www.banhcafe.hn/_next/image?url=%2Fannouncement-4.webp&w=640&q=75"
+                alt="banca"
+              /> */
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,25 +1,27 @@
 const Navbar = () => {
   return (
     <div>
-      <div className="mx-auto px-3 md:px-0 w-full py-[50px]">
+      <div className="mx-auto md:px-0 w-full py-[50px]">
         <div className="max-w-[1240px] mx-auo">
-          <div className="grid md:grid-cols-3 gap-8 md:gap-6 px-2 text-black">
-            <div className="bg-white overflow-hidden drop-shadow-md shadow-xl rounded-xl text-left hover:shadow hover:ease-out hover:duration-200">
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-1 md:gap-6 md:px-2 text-black text-sm">
+            <div className="bg-white -mx-4 overflow-hidden drop-shadow-md shadow-xl rounded-xl text-left hover:shadow hover:ease-out hover:duration-200">
               <img
-                className="h-56 w-full object-cover"
+                className="h-30 w-full object-cover"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/800px-A_small_cup_of_coffee.JPG"
                 alt="coffee"
               />
-              <div className="ms-6 my-4 pe-4 lg:text-lg">
+              <div className="ms-1.5 md:ms-6 my-4 pe-4 lg:text-lg">
                 <p>Reloj de pulsera Omega Constellation 551 de la década de 1960 esfera sartén</p>
                 <img
                   className="h-20 lg:h-28 -my-6 lg:-mb-12 lg:-mt-8 -mx-2"
                   src="https://static.thenounproject.com/png/5632679-200.png"
                   alt="stars"
                 />
-                <div className="flex justify-between relative md:-mx-1 lg:mx-1 lg:my-4">
+                <div className="flex flex-wrap justify-between relative md:-mx-1 lg:mx-1 lg:my-4">
                   <p className="text-end font-bold py-2">L 3953.48</p>
-                  <button className="rounded-lg bg-RED_LIGHT px-2 py-1 md:px-2 md:py-0 text-white lg:px-3">Envio GRATIS</button>
+                  <button className="rounded-lg bg-RED_LIGHT py-1 px-1 md:px-2 md:py-0 text-white lg:px-3 w-44 ms-2">
+                    Envio GRATIS
+                  </button>
                 </div>
               </div>
             </div>
